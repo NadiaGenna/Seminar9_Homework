6ed986bad78b84ec861b9b46e3d7ab330836dfab
«Nadezhda <nadezhdagenna@mail.ru>
2022-09-08 00:30:26 +0300

    Task68 is ready

--- Task68/Program.cs
+++ Task68/Program.cs
@@ -14,5 +14,4 @@ int AckermannFunction(int numberM, int numberN)
     if (numberM > 0 && numberN > 0) return AckermannFunction(numberM - 1, AckermannFunction(numberM, numberN - 1));
     return AckermannFunction(numberM, numberN);
 }
-
 Console.WriteLine($"Функция Аккермана для чисел A({numberM},{numberN}) = {AckermannFunction(numberM, numberN)}");
